8a5b506a55036022faa1d30cfee64ae64f754097
Benita Emudianughe <emusbeny@gmail.com>
2023-06-08 16:57:00 +0100

    made file for API KEYS and a seperate file to define the function that gets the lat and long of a place, also created a go module to track dependencies

--- api_keys.go
+++ api_keys.go
@@ -1,1 +1,4 @@
 package main
+
+const openWeatherApiKey = "47948635de508c2b46f64c505c7e5b18"
+const geocodeApiKey = "pk.177502f940918f2a06b708ee23e9bc63"
---
+++ geocoding_request.go
@@ -0,0 +1,10 @@
+package main
+
+type LatLng struct {
+	Lat float64
+	Lng float64
+}
+
+func getLatLngForPlace(place string) (lat_lng LatLng, err error){
+	url := fmt.
+}
