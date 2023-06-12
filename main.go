c0b9fe38d595c86fe6c01a8e1626bee53791be8b
Benita Emudianughe <emusbeny@gmail.com>
2023-06-12 04:09:15 +0100

    having problem unmarshalling JSON

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+// defining a global variable of type http.Client from the http package
+// check documentation on it
+var Client http.Client
+
+func main() {
+	Client = http.Client{
+		//handy shortcuts for calculating time in the time package
+		Timeout: time.Second * 10,
+	}
+	getLatLng, err := getLatLngForPlace("330102") //zip code for warri
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v\n", getLatLng) //%+v is special syntax to print struct to the console
+}
---
+++ openWeather.go
@@ -0,0 +1,11 @@
+package main
+
+// import "fmt"
+
+// func getWeatherInfo(lat, lng float64) {
+// 	url := fmt.Sprintf("https://api.openweathermap.org/data/3.0/onecall?lat=%.2f&lon=%.2fd=&exclude={part}&appid=%s",
+// 		geocode.Results[0].LocationRes.Location.Lat,
+// 		geocode.Results[0].LocationRes.Location.Lng,
+// 		OpenWeatherApiKey,
+// 	)
+// }
