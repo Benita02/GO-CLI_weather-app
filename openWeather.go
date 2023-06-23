613ded8ce2e26bee162674c95513af8fc6497f86
Benita Emudianughe <emusbeny@gmail.com>
2023-06-23 23:08:56 +0100

    creating separate sturct to handle api info variable

--- openWeather.go
+++ openWeather.go
@@ -2,10 +2,31 @@ package main
 
 import "fmt"
 
+type OpenWeatherResponseCurrent struct{
+	Dt int64
+	Sunrise int64
+	Sunset int64
+	Temp float32
+	Feels_like float32
+	Pressure int
+	Humidity int
+	Dew_point float32
+	Uvi float32
+	Clouds int
+	Visibility int
+	Wind_speed float32
+	Wind_deg int
+	Weather []OpenWeatherCondition //an array that'll be defined later
+	Rain struct {
+		
+	}
+
+	
+}
 type OpenWeatherResponseOneCall struct{
 	Current *OpenWeatherResponseCurrent 
 	Hourly *[]OpenWeatherResponseHourly
-	Daily *[]OpenWeatherResponseDaily]
+	Daily *[]OpenWeatherResponseDaily
 }
 
 func getWeatherInfo(lat_lng LatLng, units string, period string) {weather OpenweatherResponseOneCall, err error}
