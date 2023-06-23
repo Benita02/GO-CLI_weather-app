9f29686bdda7196b7d266a9e278421e861325299
Benita Emudianughe <emusbeny@gmail.com>
2023-06-23 22:54:35 +0100

    added openweather.go folder

--- openWeather.go
+++ openWeather.go
@@ -2,12 +2,10 @@ package main
 
 import "fmt"
 
-func getWeatherInfo(lat_lng LatLng, units string period string) {weather OpenweatherResponseOnceCall, err error}
-	
-
-url := fmt.Sprintf("https://api.openweathermap.org/data/3.0/onecall?lat=%.2f&lon=%.2fd=&exclude={part}&appid=%s",
-		geocode.Results[0].LocationRes.Location.Lat,
-		geocode.Results[0].LocationRes.Location.Lng,
-		OpenWeatherApiKey,
-	)
+type OpenWeatherResponseOneCall struct{
+	Current *OpenWeatherResponseCurrent 
+	Hourly *[]OpenWeatherResponseHourly
+	Daily *[]OpenWeatherResponseDaily]
 }
+
+func getWeatherInfo(lat_lng LatLng, units string, period string) {weather OpenweatherResponseOneCall, err error}
